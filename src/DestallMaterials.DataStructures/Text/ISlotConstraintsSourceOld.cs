eea369882a1cda308ad8b65775761917764e7d01
destall <dest-all@outlook.com>
2025-11-15 18:38:38 +0700

    removed old date constraints source

--- src/DestallMaterials.DataStructures/Text/ISlotConstraintsSourceOld.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DestallMaterials.WheelProtection.DataStructures.Text;
-
-public interface ISlotConstraintsSourceOld<TSymbol>
-{
-    IReadOnlyList<SlotConstraint<TSymbol>> GetConstraints(IReadOnlyList<TSymbol> currentFilling);
-
-    int Length { get; }
-}
--- src/DestallMaterials.Tests.XUnit/DateTimeContstraintsSourceTests.cs
+++ src/DestallMaterials.Tests.XUnit/DateTimeContstraintsSourceTests.cs
@@ -7,7 +7,7 @@ namespace DestallMaterials.Tests.XUnit;
 public class DateTimeContstraintsSourceTests
 {
     static readonly DateFormatting _dateFormatting = new DateFormatting();
-    static readonly DateSlotConstraintsSourceOld _source = new DateSlotConstraintsSourceOld(
+    static readonly DateSlotConstraintsSource _source = new(
         new WheelProtection.DataStructures.Time.DateTimeRange(new(2000, 1, 1), new(2020, 12, 31)),
         _dateFormatting
     );
@@ -27,7 +27,7 @@ public class DateTimeContstraintsSourceTests
         var format = _dateFormatting.ToString();
         var inputValue = date.ToString(format).ToCharArray();
 
-        var constraints = _source.GetConstraints(inputValue);
+        var getConstraints = (int i) => _source.GetSlotConstraints(i, inputValue);
 
         // every numeric slot should allow exactly the digit present in inputValue
         for (int i = 0; i < inputValue.Length; i++)
@@ -36,30 +36,13 @@ public class DateTimeContstraintsSourceTests
             // separator positions should be deterministic delimiter
             if (ch == _dateFormatting.Delimiter)
             {
-                Assert.Single(constraints[i].Options);
-                Assert.Equal(ch, constraints[i].Options[0]);
+                Assert.Single(getConstraints(i).Options);
+                Assert.Equal(ch, getConstraints(i).Options[0]);
             }
             else
             {
                 // numeric digit should be among allowed options (ideally single)
-                Assert.Contains(ch, constraints[i].Options);
-            }
-        }
-    }
-
-    [Fact]
-    public void GetConstraints_SeparatorSlots_AreDeterministic()
-    {
-        // empty filling -> constraints should still have deterministic delimiter at separator positions
-        var constraints = _source.GetConstraints([]);
-        var format = _dateFormatting.ToString();
-
-        for (int i = 0; i < format.Length; i++)
-        {
-            if (!char.IsLetter(format[i]))
-            {
-                Assert.Single(constraints[i].Options);
-                Assert.Equal(format[i], constraints[i].Options[0]);
+                Assert.Contains(ch, getConstraints(i).Options);
             }
         }
     }
@@ -69,11 +52,23 @@ public class DateTimeContstraintsSourceTests
     {
         // Use a filling that sets month = 02 and year = 2000 (leap year)
         // Format dd.MM.yyyy -> positions: [0]=d1,[1]=d2,[2]='.',[3]=m1,[4]=m2,[5]='.',[6]=y1..[9]=y4
-        var filling = new char[] { default, default, _dateFormatting.Delimiter, '0', '2', _dateFormatting.Delimiter, '2', '0', '0', '0' };
+        var filling = new char[]
+        {
+            default,
+            default,
+            _dateFormatting.Delimiter,
+            '0',
+            '2',
+            _dateFormatting.Delimiter,
+            '2',
+            '0',
+            '0',
+            '0',
+        };
 
-        var constraints = _source.GetConstraints(filling);
-
-        var dayTens = constraints[0].Options;
+        var getConstraints = (int i) => _source.GetSlotConstraints(i, filling);
+        
+        var dayTens = getConstraints(0).Options;
 
         // For February in a leap year max day is 29 -> day tens '3' should not be allowed
         Assert.DoesNotContain('3', dayTens);
