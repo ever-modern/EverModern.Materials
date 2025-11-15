1c3494a20f6eb54c1e0845f86793d0ab065a395e
destall <dest-all@outlook.com>
2025-11-15 23:12:29 +0700

    Another buggy scenario test.

--- src/DestallMaterials.Tests.XUnit/NumberMaskTests.cs
+++ src/DestallMaterials.Tests.XUnit/NumberMaskTests.cs
@@ -35,4 +35,20 @@ public class NumberMaskTests
         Assert.Equal(3, carretPosition);
         Assert.Equal([.. "1985"], mask.Slots);
     }
+
+    [Fact]
+    public void WriteTopEdgeNumber()
+    {
+        const int from = 1975;
+        const int to = 2025;
+
+        var numberConstraints = new IntegerConstraintsSource(from, to);
+
+        var mask = new Mask<char>(numberConstraints, initialSlots: [.. "2020"]);
+
+        var carretPosition = mask.AcceptChange(new(At: 3, Removed: 1, Inserted: ['5']));
+
+        Assert.Equal(4, carretPosition);
+        Assert.Equal([.. "2025"], mask.Slots);
+    }
 }
